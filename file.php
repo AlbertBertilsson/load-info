972f26956f79211c1b234316116b2addd0ad81a8
AlbertBertilsson <abbe_something@hotmail.com>
2016-05-30 10:11:04 +0200

    Update file.php

--- file.php
+++ file.php
@@ -3,7 +3,7 @@
 //echo $_GET['file'];
 $file = 'images/mammut.jpg';
 
-header('Content-Type: image/jpg');
+header('Content-Type: image/jpeg');
 header('Content-Length: ' . filesize($file));
 
 readfile($file);
