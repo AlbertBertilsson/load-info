33a06bff8729456d685b65a86e339a63acaa98c2
AlbertBertilsson <abbe_something@hotmail.com>
2016-05-30 12:06:52 +0200

    Update file.php

--- file.php
+++ file.php
@@ -4,7 +4,7 @@ $name = $_GET['name'];
 $file = $_GET['file'];
 $type = $_GET['type'];
 
-setcookie($name, $file . ':' . $type . ':' . time());
+setcookie($name, $file . ':' . $type . ':' . time() . ':' . $_SERVER['HTTP_ACCEPT'] . ':' . $_SERVER['HTTP_USER_AGENT']);
 
 header('Content-Type: ' . $type);
 
